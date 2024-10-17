73c3719c41cb6f696508c17ea11553e241fd646a
Kajsa Classon <ispyspiders@gmail.com>
2024-10-17 16:26:12 +0200

    påbörjat flödesschema för program

--- Program.cs
+++ Program.cs
@@ -21,14 +21,21 @@ namespace ptApp
                 Console.CursorVisible = false;
                 Console.WriteLine("T R Ä N I N G S D A G B O K \n\n");
 
-                Console.WriteLine("1. ");
+                Console.WriteLine("1. Logga in");
+                Console.WriteLine("2. Skapa ny användare");
 
                 Console.WriteLine("\nX. Avsluta\n");
 
                 int input = (int)Console.ReadKey(true).Key;
                 switch (input)
                 {
-                    case '1':
+                    case '1': // Logga in
+                        break;
+
+                    case '2': // Skapa användare
+                        Console.Clear();
+                        Console.CursorVisible = false;
+                        Console.WriteLine("S K A P A   N Y   A N V Ä N D A R E\n\n");
                         break;
 
                     case 88: // X för exit
