c50edec27fd6ec92800c45149ba979730b505deb
Kajsa Classon <ispyspiders@gmail.com>
2024-10-17 11:54:36 +0200

    Flyttat klassfiler till mapp

---
+++ Library/User.cs
@@ -0,0 +1,13 @@
+// Klass för användare
+// Skapad av Kajsa Classon, HT24.
+
+namespace ptApp
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+
+    }
+}
