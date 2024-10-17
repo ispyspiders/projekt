0496532b8d38354232b506c4191031523eb8a15d
Kajsa Classon <ispyspiders@gmail.com>
2024-10-17 09:24:34 +0200

    Skapat huvudmeny

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Projektuppgift i kursen Programmering i C#.NET (DT071G) vid Mittuniveristetet
+// Skapad av Kajsa Classon, HT24
+// Beskrivning
+
+using System;
+using Microsoft.Data.Sqlite;
+
+
+namespace ptApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                // Skriv ut meny
+                Console.Clear();
+                Console.CursorVisible = false;
+                Console.WriteLine("T R Ä N I N G S D A G B O K \n\n");
+
+                Console.WriteLine("1. ");
+
+                Console.WriteLine("\nX. Avsluta\n");
+
+                int input = (int)Console.ReadKey(true).Key;
+                switch (input)
+                {
+                    case '1':
+                        break;
+
+                    case 88: // X för exit
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+        }
+    }
+}
