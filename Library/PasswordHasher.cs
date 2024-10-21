413404885e2d50381e73e103b12ccf2240b60eab
Kajsa Classon <ispyspiders@gmail.com>
2024-10-21 14:56:44 +0200

    Skapat funktion för validering av lösenord

--- Library/PasswordHasher.cs
+++ Library/PasswordHasher.cs
@@ -1,3 +1,5 @@
+// Lösenords hashare enligt tutorial på : https://yarkul.com/hash-salt-store-password-in-csharp/
+
 using System.Security.Cryptography;
 
 namespace ptApp
@@ -16,6 +18,16 @@ namespace ptApp
             var hash = Rfc2898DeriveBytes.Pbkdf2(password, salt, Iterations, _hashAlgorithmName, KeySize);
 
             return string.Join(SaltDelimiter, Convert.ToBase64String(salt), Convert.ToBase64String(hash));
+        }
+
+        public bool Validate(string passwordHash, string password)
+        {
+            var passwordElements = passwordHash.Split(SaltDelimiter);
+            var salt = Convert.FromBase64String(passwordElements[0]);
+            var hash = Convert.FromBase64String(passwordElements[1]);
+            var hashInput = Rfc2898DeriveBytes.Pbkdf2(password, salt, Iterations, _hashAlgorithmName, KeySize);
+
+            return CryptographicOperations.FixedTimeEquals(hash, hashInput);
         }
 
     }
