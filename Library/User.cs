5180dd41a7d8c34040bac7916b8b67c248c817e4
Kajsa Classon <ispyspiders@gmail.com>
2024-10-22 15:12:59 +0200

    Flyttat funktioner som rör användare tillö klass för användare. Registrera användare, hämta användarnamn och logga in användare.

--- Library/User.cs
+++ Library/User.cs
@@ -1,5 +1,7 @@
 // Klass för användare
 // Skapad av Kajsa Classon, HT24.
+using System.Data;
+using Microsoft.Data.Sqlite; // Installeras med: 'dotnet add package Microsoft.Data.Sqlite'
 
 namespace ptApp
 {
@@ -7,7 +9,91 @@ namespace ptApp
     {
         public int Id { get; set; }
         public string? Username { get; set; }
-        public string? Password { get; set; }
+        private string? Password { get; set; }
+
+        private PtApp ptApp = new PtApp(); // DB-connection
+
+        // Registrera en ny användare
+        public bool RegisterUser(string username, string password)
+        {
+            using (var connection = new SqliteConnection(ptApp.connectionString))
+            {
+                PasswordHasher passwordhasher = new PasswordHasher();
+                string passwordHash = passwordhasher.Hash(password);
+
+                // Öppna connection
+                connection.Open();
+
+                string query = @"INSERT INTO users (username, password)
+                                VALUES
+                                (@Username, @PasswordHash);";
+
+                using (var command = new SqliteCommand(query, connection))
+                {
+                    command.Parameters.Add(new SqliteParameter("@Username", username));
+                    command.Parameters.Add(new SqliteParameter("@PasswordHash", passwordHash));
+
+                    int result = command.ExecuteNonQuery();
+                    if (result >= 1) return true;
+                    else return false;
+                }
+            }
+        }
+
+        // Hämta användare för kontroll om användarnamn finns i db
+        public bool GetUserByName(string username)
+        {
+            using (var connection = new SqliteConnection(ptApp.connectionString))
+            {
+                // Öppna connection
+                connection.Open();
+
+                string query = $@"SELECT username FROM users WHERE username=@Username;";
+                // Skicka query till db
+                using (var command = new SqliteCommand(query, connection))
+                {
+                    command.Parameters.Add(new SqliteParameter("@Username", username));
+
+                    using var reader = command.ExecuteReader();
+                    if (reader.HasRows) return true;
+                    else return false;
+                }
+            }
+        }
+
+        // Logga in användare
+        public bool LoginUser(string username, string passwordInput)
+        {
+            try
+            {
+                using (var connection = new SqliteConnection(ptApp.connectionString))
+                {
+                    // Öppna connection
+                    connection.Open();
+                    // Läs in lösenord för användare med valt användarnamn
+                    string query = $@"SELECT password FROM users WHERE username=@Username;";
+                    // Skicka query till db
+                    using (var command = new SqliteCommand(query, connection))
+                    {
+                        command.Parameters.Add(new SqliteParameter("@Username", username));
+                        var retrievedPassword = command.ExecuteScalar() as string;
+                        if (!String.IsNullOrEmpty(retrievedPassword))
+                        {
+                            PasswordHasher passwordhasher = new PasswordHasher();
+                            bool result = passwordhasher.Validate(retrievedPassword, passwordInput);
+                            return result;
+                        } else return false;
+                    }
+                }
+            }
+            catch (SqliteException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+
+
+        }
 
     }
 }
